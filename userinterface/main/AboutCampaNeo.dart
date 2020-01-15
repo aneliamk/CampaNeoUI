c8a438a6c3019f26946ff997b8bc35d629a51c38
aneliamk <a.kurteva1516@gmail.com>
2020-01-15 16:09:25 +0100

    Update AboutCampaNeo.dart

--- userinterface/main/AboutCampaNeo.dart
+++ userinterface/main/AboutCampaNeo.dart
@@ -12,13 +12,76 @@ class About extends StatelessWidget {
       appBar: AppBar(
         title: Text("About CampaNeo"),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: Text('Go back!'),
+      body:
+      //add campaneo describtion before the table
+      Container(
+        margin: EdgeInsets.only(top: 100, bottom: 100, left: 150, right: 150),
+        padding:
+            const EdgeInsets.only(left: 100, right: 100, top: 50, bottom: 50),
+        decoration: BoxDecoration(
+            border: Border.all(
+          color: Colors.blue,
+          width: 10,
+        )),
+        child: Table(
+          border: TableBorder.all(color: Colors.white),
+          children: [ 
+            TableRow(children: [
+              Container(
+                color: Colors.white,
+                child: Image.asset(
+                  'logo/volkswagen.jpg',
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                child: Image.asset(
+                  'logo/sti.png',
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                child: Image.asset(
+                  'logo/LB.png',
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+            ]),
+            TableRow(children: [
+              Container(
+                color: Colors.white,
+                child: Image.asset(
+                  'logo/moma.png',
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                child: Image.asset(
+                  'logo/ag.png',
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                child: Image.asset(
+                  'logo/ctsx.png',
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+            ])
+          ],
         ),
+   
+            
       ),
     );
   }
